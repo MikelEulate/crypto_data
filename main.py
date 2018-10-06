f54d7895849f39eb4ec5846eed048ec3dd2e2ebf
MikelEulate <35065103+MikelEulate@users.noreply.github.com>
2018-10-06 18:39:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ from matplotlib import style
 
 from bs4 import BeautifulSoup
 
+import argparse
+
+parser = argparse.ArgumentParser(description='This script is made to retrieve historical cryptocurrencies data from Binance')
+
+parser.add_argument('--I', dest='initial_date',
+                    help='initial date to retrieve data')
+parser.add_argument('--F', dest='final_date',
+                    help='Final date to retrieve data')
+
+parser.add_argument('--Freq', dest='freq',
+                    help='Frequency of de data stored')
+
+# The arguments will be saved into args variable
+args = parser.parse_args()
+
+print(args)
 
 ## Converting the open_time and close_time to a readable timestamps
 def conversion_date(timestamp):
@@ -65,7 +81,52 @@ for crypto in crypto_portfolio:
 
 i = 0
 for crypto in crypto_search_tickers:
-    klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1HOUR , "1 Dec, 2017", "25 Sept, 2018")
+
+    if args.freq == '1m':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1MINUTE , args.initial_date, args.final_date)
+
+    if args.freq == '3m':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_3MINUTE , args.initial_date, args.final_date)
+
+    if args.freq == '5m':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_5MINUTE , args.initial_date, args.final_date)
+
+    if args.freq == '15m':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_15MINUTE , args.initial_date, args.final_date)
+
+    if args.freq == '30m':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_30MINUTE , args.initial_date, args.final_date)
+
+    if args.freq == '1h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '2h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_2HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '4h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_4HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '6h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_6HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '8h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_8HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '12h':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_12HOUR , args.initial_date, args.final_date)
+
+    if args.freq == '1d':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1DAY , args.initial_date, args.final_date)
+
+    if args.freq == '3d':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_3DAY , args.initial_date, args.final_date)
+
+    if args.freq == '1w':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1WEEK , args.initial_date, args.final_date)
+
+    if args.freq == '1M':
+        klines = client.get_historical_klines(crypto, Client.KLINE_INTERVAL_1MONTH , args.initial_date, args.final_date)
+
     
     df = pd.DataFrame(klines, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',\
                                    'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', \
@@ -84,4 +145,5 @@ for crypto in crypto_search_tickers:
         df_all.loc[:,crypto] = pd.Series(df[crypto], index=df.index)
     i += 1
 
-df_all.to_csv('crypto_df_1H_prices'+ str(int(time.time()))+'.csv')
+name_df = f"/home/mikel/mikel_wind/BINANCE/data/crypto_df_{args.initial_date}-{args.final_date}-FREQ-{args.freq}"
+df_all.to_csv(name_df+'.csv')
